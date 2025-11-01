1b2f6705856fbce79bb35d08cd722f4fbb2d9aa7
5salihou <csalikh@gmail.com>
2025-11-01 22:53:54 +0000

    Configuration de Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swaguer
+  const config = new DocumentBuilder()
+    .setTitle('mini-api-dexchange')
+    .setDescription('une mini-API de gestion de transferts sécurisée')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.setGlobalPrefix('api'); // L'ajout d'un préfixe pour les routes de l'API.
 
